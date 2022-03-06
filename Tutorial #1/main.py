be2d2bd05e86c6225fbce6b25281c7813ece4946
G4rutti <89161614+G4rutti@users.noreply.github.com>
2022-03-06 18:15:09 -0300

    Add files via upload

---
+++ Tutorial #1/main.py
@@ -0,0 +1,11 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+
+class RogerioApp(App):
+    def build(self):
+        return Label(text='Hello World', font_size=72)
+
+
+if __name__ == '__main__':
+    RogerioApp().run()
---
+++ Tutorial #2/Inputs_and_Buttons.py
@@ -0,0 +1,58 @@
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+
+class MyGridLayout(GridLayout):
+    #Iniciar infinitas palavras chaves
+    def __init__(self, **kwargs):
+        #Chamar o construtor grid layout
+        super(MyGridLayout, self).__init__(**kwargs)
+
+        #set columns
+        self.cols = 2
+
+        #add widgets
+        self.add_widget(Label(text='Nome: '))
+        #add input box
+        self.nome = TextInput(multiline=False)
+        self.add_widget(self.nome)
+        
+        #add widgets
+        self.add_widget(Label(text='Pizza favorita: '))
+        #add input box
+        self.pizza = TextInput(multiline=False)
+        self.add_widget(self.pizza)
+
+        #add widgets
+        self.add_widget(Label(text='Time do Coração: '))
+        #add input box
+        self.time = TextInput(multiline=False)
+        self.add_widget(self.time)
+
+        #Criar o Botão
+        self.enviar = Button(text='Enviar', font_size = 32)
+        #Bind(ligar) the button
+        self.enviar.bind(on_press=self.press_func)
+        self.add_widget(self.enviar)
+
+    def press_func(self, instance):
+        name = self.nome.text
+        pizza = self.pizza.text
+        club = self.time.text
+
+        self.add_widget(Label(text=f'Olá, {name}! Você torce para o(a) {club} e gosta da pizza de {pizza}'))
+        
+        #Clear the input boxes
+        self.nome.text = ''
+        self.pizza.text = ''
+        self.time.text = ''
+
+class MyAppApp(App):
+    def build(self):
+        return MyGridLayout()
+
+
+if __name__ == '__main__':
+    MyAppApp().run()
